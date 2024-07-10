204b7f30ed755de301fb1d045119e8fc5dd10a57
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-07-11 01:06:27 +0600

    modify baseQuery for sending accesstoken with every api call

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,24 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
+
+const baseQuery = fetchBaseQuery({
+    baseUrl:'http://localhost:5022/api/v1',
+    credentials:'include',
+    prepareHeaders:(headers,{getState})=>{
+        const token = (getState() as RootState).auth.token;
+        if(token){
+            headers.set('authorization',token);
+        }
+        return headers;
+    }
+})
 
 
 
 export const baseApi = createApi({
     reducerPath:'baseApi',
-    baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5022/api/v1'}),
+    baseQuery,
     endpoints:()=>({})
 })
 
